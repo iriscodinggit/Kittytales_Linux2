100a39ca7f6754a01fd7ce457aee39880e3784e5
Iris <133221081+iriscodinggit@users.noreply.github.com>
2024-06-27 13:41:27 +0200

    Update ft_printf.h

--- headers/ft_printf.h
+++ headers/ft_printf.h
@@ -3,29 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: flopez-r <flopez-r@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: irlozano <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/27 12:04:50 by fabriciolop       #+#    #+#             */
-/*   Updated: 2023/11/16 16:40:42 by flopez-r         ###   ########.fr       */
+/*   Created: 2023/11/11 13:40:27 by irlozano          #+#    #+#             */
+/*   Updated: 2023/11/25 14:44:22 by irlozano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-# include <stdarg.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <unistd.h>
+# include <stdarg.h> //librería de las variadic
+# include <unistd.h> //librería de write
+# include <stdlib.h> //creo que libreria de size_t
 
-void	ft_putnbr_unsigned(unsigned int n, int *count);
-void	ft_print_memory(unsigned long i, int *count);
-void	ft_converter_hexa_long(unsigned long number, int *count);
-void	ft_converter_hexa_min(unsigned int number, int *count);
-void	ft_converter_hexa_mayus(unsigned int number, int *count);
-void	ft_putchar(char c, int *count);
-void	ft_putstr(char *s, int *count);
-void	ft_putnbr(int n, int *count);
-int		ft_printf(char const *values, ...);
+int		ft_printf(char const *str, ...);
+void	ft_format(va_list arguments, char format, size_t *counter);
 
-#endif // ft_printf
+//utils:
+void	ft_putchar(char c, size_t *counter);
+void	ft_putstr(char *s, size_t *counter);
+void	ft_putnbr(long n, size_t *counter);
+void	ft_putpointer(unsigned long pointer, size_t *counter);
+void	ft_puthexa(unsigned int nbr, size_t *counter, char case_type);
+
+#endif
